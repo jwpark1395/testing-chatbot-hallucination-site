f823e730e67abad71d984e803b800dc20a14eb2c
Sungwon Hwang <shwang.14@kaist.ac.kr>
2025-02-09 17:21:49 +0900

    d

--- contents/post_interaction.py
+++ contents/post_interaction.py
@@ -215,8 +215,8 @@ def save_results():
 
 
     # Call the function to send email
-    # send_results_email(data_to_save, "jwp14812@snu.ac.kr")
-    send_results_email(data_to_save, "sungwonhwang95@gmail.com")
+    send_results_email(data_to_save, "jwp14812@snu.ac.kr")
+    # send_results_email(data_to_save, "sungwonhwang95@gmail.com")
 
 
 
