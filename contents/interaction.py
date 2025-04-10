2d2353c7b5bb38132ab5b4654ea508a5da850270
jwpark1395 <86582197+jwpark1395@users.noreply.github.com>
2025-04-11 01:20:18 +0900

    Update interaction.py

--- contents/interaction.py
+++ contents/interaction.py
@@ -188,7 +188,7 @@ def interaction_page():
                     height: 100vh;
                 }
                 div[data-testid="stVerticalBlockBorderWrapper"]:has(> div > .st-key-intro) {
-                    height: 150px;
+                    height: 100px;
                 }
                 div:has(> .st-key-intro) {
                     height: 100%;
